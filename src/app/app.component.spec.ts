3c62a2a04013499b3b1dfeb3f7464732bfda2afc
kragmar <kragmar97@gmail.com>
2020-03-16 18:16:39 +0100

    bug(testing): fix missing imports from spec files

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,12 +1,14 @@
 import { TestBed, async } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar.component';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule
+        RouterTestingModule,
+        NavbarComponent
       ],
       declarations: [
         AppComponent
@@ -20,16 +22,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'ELTÉvedtem'`, () => {
+  it(`should have as title 'angular-elteik-knowledgebase'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('ELTÉvedtem');
+    expect(app.title).toEqual('angular-elteik-knowledgebase');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('ELTÉvedtem app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('angular-elteik-knowledgebase app is running!');
   });
 });
