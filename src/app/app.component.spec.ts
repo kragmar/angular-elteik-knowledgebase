53b50e311b2db34dd74177d044335ae1a34cb996
kragmar <kragmar97@gmail.com>
2020-03-16 16:08:07 +0100

    minor changes

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'angular-elteik-knowledgebase'`, () => {
+  it(`should have as title 'ELTÉvedtem'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular-elteik-knowledgebase');
+    expect(app.title).toEqual('ELTÉvedtem');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('angular-elteik-knowledgebase app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('ELTÉvedtem app is running!');
   });
 });
